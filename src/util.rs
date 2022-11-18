01eb9d0c45d7d96645acb28f339980133be3bab7
laralove <laralove143@icloud.com>
2022-11-18 22:28:55 +0300

    Add methods for interactions

--- src/util.rs
+++ src/util.rs
@@ -1,7 +1,11 @@
 use std::fmt::Debug;
 
 use titlecase::titlecase;
-use twilight_model::guild::Permissions;
+use twilight_model::{
+    application::interaction::{Interaction, InteractionData},
+    guild::Permissions,
+    user::User,
+};
 
 /// Implemented on types that can be turned into pretty strings
 pub trait Prettify: Debug {
@@ -32,5 +36,40 @@ impl Prettify for Permissions {
         }
 
         titlecase(&format!("{self:?}").replace(" | ", "\n").replace('_', " "))
+    }
+}
+
+/// Utility methods for
+/// [`twilight_model::application::interaction::Interaction`]
+trait InteractionExt {
+    /// Return the name or custom ID of the interaction
+    ///
+    /// Returns `None` when called on a
+    /// [`twilight_model::application::interaction::InteractionType::Ping`]
+    /// interaction
+    fn name(&self) -> Option<&str>;
+
+    /// Return the user of the interaction, whether it's in DMs or not
+    ///
+    /// Should never return `None`
+    fn user(&self) -> Option<&User>;
+}
+
+impl InteractionExt for Interaction {
+    fn name(&self) -> Option<&str> {
+        Some(match self.data? {
+            InteractionData::ApplicationCommand(data) => &data.name,
+            InteractionData::MessageComponent(data) => &data.custom_id,
+            InteractionData::ModalSubmit(data) => &data.custom_id,
+            _ => None?,
+        })
+    }
+
+    fn user(&self) -> Option<&User> {
+        Some(
+            self.user
+                .as_ref()
+                .unwrap_or_else(|| self.member.as_ref()?.user.as_ref()?),
+        )
     }
 }
