cf2768e4cda0fc5f84446b29fca7115f0ba2af6e
laralove <laralove143@icloud.com>
2022-12-07 01:51:49 +0300

    Add convenience methods for HTTP errors

--- src/util.rs
+++ src/util.rs
@@ -2,7 +2,10 @@ use std::fmt::Debug;
 
 use async_trait::async_trait;
 use titlecase::titlecase;
-use twilight_http::request::channel::message::CreateMessage;
+use twilight_http::{
+    api_error::{ApiError, GeneralApiError},
+    request::channel::message::CreateMessage,
+};
 #[cfg(doc)]
 use twilight_model::application::interaction::InteractionType;
 use twilight_model::{
@@ -158,5 +161,42 @@ impl HttpExt for twilight_http::Client {
             .id;
 
         Ok(self.create_message(channel_id))
+    }
+}
+
+/// Utility methods for [`twilight_http::Error`]
+pub trait HttpErrorExt {
+    /// Return the [`GeneralApiError`] code of the error, returns `None` if the
+    /// error is not a [`GeneralApiError`]
+    fn code(self) -> Option<u64>;
+
+    /// Return whether this error is related to missing permissions
+    fn missing_permissions(self) -> bool;
+
+    /// Return whether this error is an `Unknown message` error, useful to check
+    /// if the error occurred because the message was deleted before the method
+    /// was sent
+    fn unknown_message(self) -> bool;
+}
+
+impl HttpErrorExt for twilight_http::Error {
+    fn code(self) -> Option<u64> {
+        if let twilight_http::error::ErrorType::Response {
+            error: ApiError::General(GeneralApiError { code, .. }),
+            ..
+        } = self.kind()
+        {
+            Some(*code)
+        } else {
+            None
+        }
+    }
+
+    fn missing_permissions(self) -> bool {
+        self.code().map_or(false, |code| code == 50013)
+    }
+
+    fn unknown_message(self) -> bool {
+        self.code().map_or(false, |code| code == 10008)
     }
 }
