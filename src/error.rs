7987b5512dc2542065a204ba7cc222f023969442
laralove <laralove143@icloud.com>
2023-02-04 22:04:56 +0300

    Flatten conversion methods

--- src/error.rs
+++ src/error.rs
@@ -1,18 +1,35 @@
 use std::{
+    any::type_name,
     error::Error,
     fmt::{Debug, Display, Formatter},
 };
 
+use anyhow::anyhow;
 use twilight_model::guild::Permissions;
 
 use crate::error::extract::HttpErrorExt;
 #[cfg(doc)]
 use crate::{interaction::InteractionHandle, Bot};
 
-/// Converting errors to other types (namely `Option`) into a `Result`
-pub mod conversion;
 /// Extracting data from Twilight's errors
 pub mod extract;
+
+/// Trait implemented on types that can be converted into an [`anyhow::Error`]
+#[allow(clippy::module_name_repetitions)]
+pub trait IntoError<T>: Sized {
+    /// Conditionally wrap this type in [`anyhow::Error`]
+    ///
+    /// The error message only includes the type info and isn't very useful
+    /// without enabling backtrace
+    #[allow(clippy::missing_errors_doc)]
+    fn ok(self) -> Result<T, anyhow::Error>;
+}
+
+impl<T> IntoError<T> for Option<T> {
+    fn ok(self) -> Result<T, anyhow::Error> {
+        self.ok_or_else(|| anyhow!("{} is None", type_name::<Self>()))
+    }
+}
 
 /// A user-facing error
 ///
